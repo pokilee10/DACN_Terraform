71a584329a9abfdc55d819c6a7b7a3e50576d220
Lê Xuân Linh <113287392+pokilee10@users.noreply.github.com>
2024-06-21 04:25:42 +0700

    update destroy ecs

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ec2mytfstate6"  # Tên của bucket S3 bạn đã tạo
+    bucket = "ecss3bucket"  # Tên của bucket S3 bạn đã tạo
     key    = "state/terraform.tfstate"  # Đường dẫn đến file trạng thái
     region = "us-east-2"  # Vùng AWS mà bucket S3 của bạn nằm trong đó
   }
